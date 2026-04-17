3979cb341a4f00706c2f89154d872ab9bf7fef1a
ShenSudara <sudarakainfo2000@gmail.com>
2026-04-17 15:07:54 +0530

    Initial Commit

---
+++ CampusMate/CampusMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusMateApp.swift
+//  CampusMate
+//
+//  Created by Ashen Sudaraka on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct CampusMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
